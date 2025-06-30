cacd0f433b7f85734cad8427db9c90ca389057e2
amadeusedu <nafiu0001@gmail.com>
2025-06-30 11:05:40 +1000

    Update gateway.tf

--- infra/gateway/gateway.tf
+++ infra/gateway/gateway.tf
@@ -3,8 +3,6 @@ resource "aws_apigatewayv2_api" "dev_api" {
   protocol_type = "HTTP"
   tags          = local.tags
 }
-
-resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.dev_api.id
   name        = "$default"
   auto_deploy = true
