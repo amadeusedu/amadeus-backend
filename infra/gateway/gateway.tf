2fb4ee6c3e329906cf624a72c19ee8728e29151c
amadeusedu <nafiu0001@gmail.com>
2025-06-30 11:19:22 +1000

    Update gateway.tf

--- infra/gateway/gateway.tf
+++ infra/gateway/gateway.tf
@@ -6,8 +6,6 @@ resource "aws_apigatewayv2_api" "dev_api" {
   protocol_type = "HTTP"
   tags          = local.tags
 }
-
-resource "aws_apigatewayv2_integration" "status_integration" {
   api_id                 = aws_apigatewayv2_api.dev_api.id
   integration_type       = "AWS_PROXY"
   integration_uri        = aws_lambda_function.status_fn.invoke_arn
@@ -15,7 +13,6 @@ resource "aws_apigatewayv2_integration" "status_integration" {
   payload_format_version = "2.0"
 }
 
-resource "aws_apigatewayv2_route" "status_route" {
   api_id    = aws_apigatewayv2_api.dev_api.id
   route_key = "GET /status"
   target    = "integrations/${aws_apigatewayv2_integration.status_integration.id}"
