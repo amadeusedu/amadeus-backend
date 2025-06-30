47141a41969900a3afe42ec84e2fe5c538c10e4e
amadeusedu <nafiu0001@gmail.com>
2025-06-30 10:26:28 +1000

    Update stage_jwt.tf
    resource "aws_apigatewayv2_stage" "default" { removed - to avoid redeclaration

--- infra/gateway/stage_jwt.tf
+++ infra/gateway/stage_jwt.tf
@@ -1,5 +1,4 @@
 # Apply JWT authorizer globally by default
-resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.dev_api.id
   name        = "$default"
   auto_deploy = true
